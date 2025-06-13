89d4234dc141655bc23845aa0415b0fbdcb8da24
Tetiana5Buria <poriniya@gmail.com>
2025-06-13 17:31:32 +0300

    added matador

--- src/components/Matador.tsx
+++ src/components/Matador.tsx
@@ -1,1 +1,34 @@
-export const Matador = () => <div>i am matador</div>
+
+ import "./matador.css";
+/*import React, { useEffect, useState } from 'react'; */
+export const Matador = () => (
+    <div className="canvas">
+      <div className="bodyMatador">
+        <div className="matador-head">
+          <div className="matador-hair"></div>
+          <div className="matador-eyes">
+            <div className="matador-eye left"></div>
+            <div className="matador-eye right"></div>
+          </div>
+          <div className="matador-mustache">
+            <div className="mustache left"></div>
+            <div className="mustache right"></div>
+          </div>
+          <div className="matador-mouth"></div>
+        </div>
+        <div className="matador-arms">
+          <div className="matador-arm left"></div>
+          <div className="matador-arm right"></div>
+        </div>
+        <div className="red-flag"></div>
+        <div className="matador-legs">
+          <div className="matador-leg left"></div>
+          <div className="matador-leg right"></div>
+        </div>
+      </div>
+    </div>
+  );
+
+
+
+/* export const Matador = () => <div className="matador">i am matador</div> */
